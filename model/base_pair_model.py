669154c5d1fac61108e45c500afc1a59cd7e17a4
marjanalbouye <marjan.albouye73@gmail.com>
2024-01-03 12:18:49 -0700

    finish model and input prep

--- model/base_pair_model.py
+++ model/base_pair_model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import rowan
-from rotation_matrix_ops import dot_product, cross_product
+import rotation_matrix_ops as rmo
 
 
 class BasePairNN(nn.Module):
@@ -17,7 +17,7 @@ class BasePairNN(nn.Module):
         self.act_fn = act_fn
         self.dropout = dropout
         self.device = device
-        self.in_dim = in_dim + 2
+        self.in_dim = in_dim
         self.batch_norm = batch_norm
 
         self.energy_net = self._get_energy_net(self.energy_out_dim)
@@ -41,6 +41,7 @@ class BasePairNN(nn.Module):
         # R1: particle 1 orientations (rotation matrix 3x3)
         # R2: particle 2 orientations (rotation matrix 3x3)
 
+        batch_size = x1.shape[0]
         features = []
 
         dr = x1 - x2
@@ -48,86 +49,62 @@ class BasePairNN(nn.Module):
         dr = dr / R
         inv_r = 1. / R
 
-        features.extend([dr, R, inv_r])
+        orient_dot_prod = rmo.dot_product(R1, R2)
+        orient_cross_prod, orient_cross_prod_norm = rmo.cross_product(R1, R2)
+        rel_orient = rmo.relative_orientation(R1, R2)
+        rel_pos_align, rel_pos_project = rmo.rel_pos_orientation(dr, R1, R2)
+        rbf = rmo.RBF_rel_pos(dr, R1, R2)
+        angle = rmo.rot_matrix_to_angle(rel_orient)
 
-        features.append(dot_product(R1, R2))
-        features.append(cross_product(R1, R2))
-        
-
-
-
-
-
-        # calculate dr vector in rotated frame
-        dr_rot = torch.matmul(R1, dr.unsqueeze(-1)).squeeze(-1)
-
-
-    def _prep_input_quat(self, x1, x2, q1, q2):
-        # x1: particle 1 positions
-        # x2: particle 2 positions
-        # q1: particle 1 orientations (quaternion)
-        # q2: particle 2 orientations (quaternion)
-
-        dr = x1 - x2
-        R = torch.norm(dr, dim=1, keepdim=True)
-        dr = dr / R
-
-        # convert dr vector to a quaternion
-        dr_q = torch.tensor(rowan.normalize(torch.cat((torch.zeros(dr.shape[0], 1), dr), dim=1)))
-
-
-        # calculate q1 applied to q2 and vice versa
-        q1q2 = torch.tensor(rowan.multiply(q1, rowan.conjugate(q2)))
-
-
-        # rotate dr based on q1 and q2 and q1q2
-        dr_q1 = torch.tensor(rowan.multiply(rowan.multiply(q1, dr_q),
-                                            rowan.conjugate(q1)))
-        dr_q2 = torch.tensor(rowan.multiply(rowan.multiply(q2, dr_q),
-                                            rowan.conjugate(q2)))
-        dr_q1q2 = torch.tensor(rowan.multiply(rowan.multiply(q1q2, dr_q),
-                                              rowan.conjugate(q1q2)))
-
-
-        features = torch.concatenate(
-            (R, 1./R, dr, q1, q2, q1q2, dr_q1, dr_q2, dr_q1q2), dim=1)
-
+        features = torch.cat((R, inv_r, dr, orient_dot_prod,
+                              orient_cross_prod.reshape(batch_size, -1),
+                              orient_cross_prod_norm,
+                              rel_orient.reshape(batch_size, -1),
+                              rel_pos_align,
+                              rel_pos_project.reshape(batch_size, -1),
+                              rbf, angle),
+                             dim=1)
 
         return features.to(self.device)
 
-
-class PairNN(BasePairNN):
-    def __init__(self, in_dim, hidden_dim, out_dim, n_layers, **kwargs):
-        super(PairNN, self).__init__(in_dim, hidden_dim, out_dim, n_layers,
-                                     **kwargs)
-
-    def _get_energy_net(self):
-        layers = [nn.Linear(self.in_dim, self.hidden_dim), self._get_act_fn()]
-        for i in range(self.n_layers - 1):
-            layers.append(nn.Linear(self.hidden_dim, self.hidden_dim))
-            if self.batch_norm:
-                layers.append(nn.BatchNorm1d(self.hidden_dim))
-            layers.append(self._get_act_fn())
-            layers.append(nn.Dropout(p=self.dropout))
-        layers.append(nn.Linear(self.hidden_dim, self.out_dim))
-        return nn.Sequential(*layers)
-
-    def forward(self, x1, x2, q1, q2):
-        x = self._prep_input(x1, x2, q1, q2)
-        energy = self.force_net(x)
-        force = -torch.autograd.grad(energy.sum(),
-                                     x1,
-                                     create_graph=True)[0]
-        torque = self.torque_net(x)
-        return force, torque
+    # def _prep_input_quat(self, x1, x2, q1, q2):
+    #     # x1: particle 1 positions
+    #     # x2: particle 2 positions
+    #     # q1: particle 1 orientations (quaternion)
+    #     # q2: particle 2 orientations (quaternion)
+    #
+    #     dr = x1 - x2
+    #     R = torch.norm(dr, dim=1, keepdim=True)
+    #     dr = dr / R
+    #
+    #     # convert dr vector to a quaternion
+    #     dr_q = torch.tensor(rowan.normalize(
+    #         torch.cat((torch.zeros(dr.shape[0], 1), dr), dim=1)))
+    #
+    #     # calculate q1 applied to q2 and vice versa
+    #     q1q2 = torch.tensor(rowan.multiply(q1, rowan.conjugate(q2)))
+    #
+    #     # rotate dr based on q1 and q2 and q1q2
+    #     dr_q1 = torch.tensor(rowan.multiply(rowan.multiply(q1, dr_q),
+    #                                         rowan.conjugate(q1)))
+    #     dr_q2 = torch.tensor(rowan.multiply(rowan.multiply(q2, dr_q),
+    #                                         rowan.conjugate(q2)))
+    #     dr_q1q2 = torch.tensor(rowan.multiply(rowan.multiply(q1q2, dr_q),
+    #                                           rowan.conjugate(q1q2)))
+    #
+    #     features = torch.concatenate(
+    #         (R, 1. / R, dr, q1, q2, q1q2, dr_q1, dr_q2, dr_q1q2), dim=1)
+    #
+    #     return features.to(self.device)
 
 
 class PairNN_Force_Torque(BasePairNN):
     def __init__(self, in_dim, hidden_dim, n_layers, **kwargs):
-        super(PairNN_Force_Torque, self).__init__(in_dim, hidden_dim, out_dim, n_layers,
-                                     **kwargs)
+        super(PairNN_Force_Torque, self).__init__(in_dim, hidden_dim,
+                                                  n_layers,
+                                                  **kwargs)
 
-    def _get_net(self):
+    def _get_energy_net(self, out_dim):
         layers = [nn.Linear(self.in_dim, self.hidden_dim), self._get_act_fn()]
         for i in range(self.n_layers - 1):
             layers.append(nn.Linear(self.hidden_dim, self.hidden_dim))
@@ -135,75 +112,24 @@ class PairNN_Force_Torque(BasePairNN):
                 layers.append(nn.BatchNorm1d(self.hidden_dim))
             layers.append(self._get_act_fn())
             layers.append(nn.Dropout(p=self.dropout))
-        layers.append(nn.Linear(self.hidden_dim, self.out_dim))
+        layers.append(nn.Linear(self.hidden_dim, out_dim))
         return nn.Sequential(*layers)
 
-    def forward(self, x1, x2, q1, q2):
-        x = self._prep_input(x1, x2, q1, q2)
-        out = self.net(x)
-        return out
+    def forward(self, x1, x2, R1, R2):
+        x = self._prep_features_rot_matrix(x1, x2, R1, R2)
+        energy = self.net(x)
+        force = -torch.autograd.grad(energy.sum(),
+                                     x1,
+                                     create_graph=True)[0]
+        torque = - torch.autograd.grad(energy.sum(),
+                                         R1,
+                                        create_graph=True)[0]
+        tq_x = torch.cross(torque[:, :, 0], R1[:, :, 0])
+        tq_y = torch.cross(torque[:, :, 1], R1[:, :, 1])
+        tq_z = torch.cross(torque[:, :, 2], R1[:, :, 2])
+        torque_out = torch.stack((tq_x, tq_y, tq_z), dim=2)
 
-class PairNNSkipShared(BasePairNN):
-    def __init__(self, in_dim, hidden_dim, out_dim, n_layers, **kwargs):
-        super(PairNNSkipShared, self).__init__(in_dim, hidden_dim, out_dim,
-                                               n_layers, **kwargs)
-        self.activations = self._get_activations()
-        self.dropouts = self._get_dropouts()
-        self.input_connection = nn.Linear(self.in_dim, self.hidden_dim)
-
-    def _get_net(self):
-        layers = [nn.Linear(self.in_dim, self.hidden_dim)]
-        for i in range(self.n_layers - 1):
-            layers.append(nn.Linear(self.hidden_dim, self.hidden_dim))
-            if self.batch_norm:
-                layers.append(nn.BatchNorm1d(self.hidden_dim))
-
-        layers.append(nn.Linear(self.hidden_dim, self.out_dim))
-        return nn.ModuleList(layers)
-
-    def _get_activations(self):
-        activations = []
-        for i in range(self.n_layers):
-            activations.append(self._get_act_fn())
-        return nn.ModuleList(activations)
-
-    def _get_dropouts(self):
-        dropouts = []
-        for i in range(self.n_layers):
-            dropouts.append(nn.Dropout(p=self.dropout))
-        return nn.ModuleList(dropouts)
-
-    def forward(self, x1, x2, q1, q2):
-        x = self._prep_input(x1, x2, q1, q2)
-        # transform input to hidden dim size
-        x_transform = self.input_connection(x)
-        for i in range(self.n_layers):
-            # add original transformed input to each layer before activation
-            x = self.activations[i](self.net[i](x) + x_transform)
-            x = self.dropouts[i](x)
-
-        out = self.net[-1](x)
-        return out
+        return force, torque_out
 
 
-class PairNNGrow(BasePairNN):
-    def __init__(self, in_dim, hidden_dim, out_dim, n_layers, **kwargs):
-        super(PairNNGrow, self).__init__(in_dim, hidden_dim, out_dim, n_layers,
-                                         **kwargs)
 
-    def _get_net(self):
-        layers = [nn.Linear(self.in_dim, self.hidden_dim[0]),
-                  self._get_act_fn()]
-        for i in range(1, len(self.hidden_dim)):
-            layers.append(nn.Linear(self.hidden_dim[i - 1], self.hidden_dim[i]))
-            if self.batch_dim:
-                layers.append(nn.BatchNorm1d(self.batch_dim))
-            layers.append(self._get_act_fn())
-            layers.append(nn.Dropout(p=self.dropout))
-        layers.append(nn.Linear(self.hidden_dim[-1], self.out_dim))
-        return nn.Sequential(*layers)
-
-    def forward(self, x1, x2, q1, q2):
-        x = self._augment_input(features, x1, x2)
-        out = self.net(x)
-        return out
--- model/rotation_matrix_ops.py
+++ model/rotation_matrix_ops.py
@@ -1,5 +1,5 @@
 import torch
-
+import pytorch3d.transforms as p3d
 
 def principal_axis_dot_product(R1, R2):
     # R1: particle 1 orientation rotation matrix (Bx 3 x 3)
@@ -40,10 +40,43 @@ def dot_product(R1, R2):
 
 
 def cross_product(R1, R2):
-    features = torch.zeros(R1.shape[0], 9, 3)
+    cross_prod = torch.zeros(R1.shape[0], 9, 3)
+    cross_prod_norms = torch.zeros(R1.shape[0], 9)
     counter = 0
     for i in range(3):
         for j in range(3):
-            features[:, counter, :] = torch.cross(R1[:, :, i], R2[:, :, j])
+            cross = torch.cross(R1[:, :, i], R2[:, :, j])
+            cross_prod[:, counter, :] = cross
+            cross_prod_norms[:, counter] = torch.norm(cross, dim=1)
             counter += 1
-    return features.reshape(R1.shape[0], -1)
+    return cross_prod, cross_prod_norms
+
+def relative_orientation(R1, R2):
+    # R1: particle 1 orientation rotation matrix (Bx 3 x 3)
+    # R2: particle 2 orientation rotation matrix (Bx 3 x 3)
+    return torch.matmul(R1, R2.transpose(1, 2))
+
+def rel_pos_orientation(dr, R1, R2):
+    rel_pos_alignment = torch.zeros(R1.shape[0], 6)
+    rel_pos_project = torch.zeros(R1.shape[0], 6, 3)
+    for i in range(3):
+        R1_dot_p = torch.einsum('ij, ij -> i', dr, R1[:, :, i])
+        R2_dot_p = torch.einsum('ij, ij -> i', dr, R2[:, :, i])
+        rel_pos_alignment[:, i] = R1_dot_p
+        rel_pos_alignment[:, i + 3] = R2_dot_p
+        rel_pos_project[:, i, :] = R1_dot_p.unsqueeze(-1) * R1[:, :, i]
+        rel_pos_project[:, i + 3, :] = R2_dot_p.unsqueeze(-1) * R2[:, :, i]
+
+    return rel_pos_alignment, rel_pos_project
+
+def RBF_rel_pos(dr, R1, R2):
+    rbf = torch.zeros(R1.shape[0], 6)
+    for i in range(3):
+        rbf[:, i] = torch.exp(-torch.norm(torch.cross(dr, R1[:, :, i]), dim=1))
+        rbf[:, i + 3] = torch.exp(-torch.norm(torch.cross(dr, R2[:, :, i]), dim=1))
+    return rbf
+
+def rot_matrix_to_angle(R):
+    # R: rotation matrix (Bx 3 x 3)
+    # returns: angle in radians (Bx 1)
+    return p3d.matrix_to_euler_angles(R, convention="XYZ")
