93990ff0c368635f8445e6c8976d5182f4a3c022
marjanalbouye <marjan.albouye73@gmail.com>
2024-02-28 14:45:21 -0700

    fix adjust periodic boundary

--- aniso_MLMD/model/neighbor_ops.py
+++ aniso_MLMD/model/neighbor_ops.py
@@ -30,8 +30,8 @@ def neighbors_distance_vector(positions, neighbor_list):
 
 def adjust_periodic_boundary(dr, box_len):
     half_box_len = box_len / 2
-    dr = torch.where(dr > half_box_len, dr - half_box_len, dr)
-    dr = torch.where(dr < -half_box_len, dr + half_box_len, dr)
+    dr = torch.where(dr > half_box_len, dr - box_len, dr)
+    dr = torch.where(dr < -half_box_len, dr + box_len, dr)
     return dr
 
 
